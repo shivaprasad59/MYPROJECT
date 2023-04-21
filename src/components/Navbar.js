2b53f3243fd00c16f5235170ba0517bab454b455
shivaprasad59 <111453221+shivaprasad59@users.noreply.github.com>
2023-04-21 17:00:11 +0530

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,80 +1,127 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState,useEffect } from "react";
 import './Navbar.css'
 import Home from './Restaurants/Home';
 import Home1 from './Instamart/Home';
 import Home2 from './Account/Home'
 import Home3 from './Cart/Home'
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
-
 const Navbar = () => {
-    const [talk,setTalk]=useState(true);
+    const [talk, setTalk] = useState(true);
+    const [going,setGoing]=useState(false);
+    const [listen, setListen] = useState(false);
     const speak = () => {
-       if(talk){
-        const synth = window.speechSynthesis;
-        const utterance = new SpeechSynthesisUtterance();
+        if (talk) {
+            const synth = window.speechSynthesis;
+            const utterance = new SpeechSynthesisUtterance();
 
-        // Retrieve the headers from the HTML document
-        const headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p");
-        headers.forEach((header) => {
-            utterance.text += header.textContent + " ";
-        });
+            // Retrieve the headers from the HTML document
+            const headers = document.querySelectorAll("li");
+            headers.forEach((header) => {
+                utterance.text += header.textContent + " ";
+            });
 
-        synth.speak(utterance);
-       }
-       setTimeout(()=>{
-
-       }, 500);
-        setTalk(false);
+            synth.speak(utterance);
+            utterance.onend=()=>{
+                setTimeout(() => {
+                    console.log("Hello");
+                    setListen(true);
+                }, 3000);
+            }
+        }
     };
-   
+
     const [test, setTest] = useState("");
-
+    const [go,setGo]=useState(false);
     const handleListen = () => {
-        const recognition = new window.webkitSpeechRecognition();
-
-
-        recognition.onresult = (event) => {
-            const transcript = event.results[0][0].transcript;
-            setTest(transcript);
-        };
-
-        recognition.start();
+        if (listen) {
+            setTimeout(()=>{
+                const synth = window.speechSynthesis;
+                const utterance = new SpeechSynthesisUtterance();
+                utterance.text+="Recognized";
+                synth.speak(utterance);
+            },10000);
+            setTimeout(()=>{
+                const recognition = new window.webkitSpeechRecognition();
+            recognition.onresult = (event) => {
+                const transcript = event.results[0][0].transcript;
+                setTest(transcript);
+            };
+            recognition.start();
+            console.log(test);
+            recognition.onend=()=>{
+                setGo(true);
+            }
+            },4000);
+        }
+        else{
+            const synth = window.speechSynthesis;
+            const utterance = new SpeechSynthesisUtterance();
+            utterance.text+="Can you speak one more time";
+            synth.speak(utterance);
+        }
     };
+
     const navigate = useNavigate();
-    const Goto = () => {
+    const Goto = async() => {
         if (test !== "") {
-            if (test === "Go to restaurants.") {
-                navigate("/Restaurants/Home.js");
+            var temp = "";
+            for (let i in test) {
+                if (test[i] !== " ") {
+                    temp += test[i];
+                } else {
+                    temp = "";
+                }
+                setTest(temp);
+                setTimeout(() => {
+                    setGoing(true);
+                }, 3000);
+                if(going){
+                    if (temp === "restaurants") {
+                        navigate("/Restaurants/Home.js");
+                    } else if (temp === "account") {
+                        navigate("/Account/Home.js");
+                    } else if (temp === "cart") {
+                        navigate("/Cart/Home.js");
+                    } else if (temp === "instamart") {
+                        navigate("/Instamart/Home.js");
+                    }
+                }
+                
             }
-            else if (test === "Go to account.") {
-                navigate("/Account/Home.js");
-            }
-            else if (test === "Go to cart.") {
-                navigate("/Cart/Home.js");
-            }
-            else if (test === "Go to instamart.") {
-                navigate("/Instamart/Home.js");
-            }
+        }
+    };
+    const speakNavigate=()=>{
+        if(go){
+            const synth = window.speechSynthesis;
+            const utterance = new SpeechSynthesisUtterance();
+             utterance.text+=("Going to the "+test+"page");
+             synth.speak(utterance);
         }
     }
     useEffect(() => {
-        Goto();
-    }, [test])
-    if(talk===false){
-        handleListen();
-    }
-  
+        if (listen) {
+            console.log("Function called..");
+            handleListen();
+        }
+        if(go){
+            console.log("Navigate");
+            Goto();
+            setListen(false);
+            if(going){
+                console.log("going to the page");
+                speakNavigate();
+            }
+        }
+    }, [listen,go,going]);
+
     return (
-        <button onClick={speak} className='main-button'>
+        <button onClick={speak}  className='main-button'>
             <>
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary mt-2">
                     <div className="container-fluid">
                         <Link className="navbar-brand" href="#">Swiggy</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
