c2a482f5003aa5eede44968e3502a9037fa8c45d
shivaprasad59 <111453221+shivaprasad59@users.noreply.github.com>
2023-05-01 14:32:05 +0530

    added user

--- src/components/Account/Home.js
+++ src/components/Account/Home.js
@@ -1,10 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import '../Account/Home.css'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 const Home = () => {
   const [click,setClick]=useState(0);
-  const [loggedin,setLoggedin]=useState(true);
+  const [loggedin,setLoggedin]=useState(false);
   const [test,setTest]=useState("");
+  const [user,setUser]=useState(false);
+  const [logindetails,setloginDetails]=useState([]);
+  
+  useEffect(()=>{
+      const fetchData=async()=>{
+        try{
+          const response=await axios.get(" http://localhost:5000/Users");
+            //  console.log(response.data);
+             setloginDetails(response.data);
+        }
+        catch(e){
+          console.log("error");
+        }
+      }
+      fetchData();
+      // setUser(true);
+  },[])
+  
+  useEffect(()=>{
+    if(logindetails.length<1){
+    setLoggedin(true);
+    }
+    else{
+      setloginDetails(logindetails[1]);
+    }
+  },[user])
   useEffect(()=>{
    if(loggedin){
     const synth=window.speechSynthesis;
@@ -48,7 +75,25 @@ const Home = () => {
   return (
     <div>
       <button onClick={handleClick}>
-          This is the home page of the accounts;
+          {
+            logindetails.map((item)=>{
+              return(
+                <div>
+                  <ul>
+                    <li>
+                      {item.username}
+                    </li>
+                    <li>
+                      {item.mobile}
+                    </li>
+                    <li>
+                      {item.address}
+                    </li>
+                  </ul>
+                </div>
+              )
+            })
+          }
       </button>
     </div>
   )
